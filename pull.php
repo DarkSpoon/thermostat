bbd80f24407471fa3b7b208016cb8ab086e5cb6f
DarkSpoon <jase.payne@gmail.com>
2013-04-11 22:53:39 +0000

    first commit

---
+++ pull.php
@@ -0,0 +1,37 @@
+<?php
+	//include login.php
+	$un="pi";
+	$pw="raspberry";
+	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");
+	
+	echo "Connected <BR>";
+	
+	$selected=mysql_select_db(thermostat) or die("Unable to select!");
+	
+	$query="SELECT * from Conditions";
+	$result=mysql_query($query);
+
+	while($row=mysql_fetch_array($result)){
+		echo "<BR>Average temp from query ".$row{'Temp'}."F<BR>";
+		$AC=$row{'AC'};
+		$Heat=$row{'Heat'};
+		$Fan=$row{'Fan'};
+
+		if($AC==1)
+			echo "AC is running<BR>";
+		else if($Heat==1)
+			echo "Heat is running<BR>";
+		if ($Fan==1)
+			echo "Fan is running<BR>";
+
+	}
+
+	$query="SELECT Target from Schedule";
+	$result=mysql_query($query);
+	while($row=mysql_fetch_array($result)){
+		echo "Target temp from query ".$row{'Target'}."F";
+	}
+
+	mysql_close($dbhandle);
+
+?>
---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+	//include login.php;
+	$un="pi";
+	$pw="raspberry";
+	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");
+	
+	echo "Connected <BR>";
+	
+	$selected=mysql_select_db(thermostat) or die("Unable to select!");
+
+	$query="DELETE FROM tablename";
+	$result=mysql_query($query);
+
+	$target = mysql_real_escape_string($_POST['Target']);
+	$query = "INSERT INTO Schedule VALUES ('1','Mon', '00:00', '23:59', '$target')";
+	$result=mysql_query($query);
+	
+	echo $result;
+
+?>
