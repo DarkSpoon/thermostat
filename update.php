702d0b8434ac2b27cf5e7ed1ec99bd1c50ba2bf3
DarkSpoon <jase.payne@gmail.com>
2013-04-11 20:52:36 -0500

    debug echos

--- update.php
+++ update.php
@@ -9,8 +9,11 @@
 	$result=mysql_query($query);
 
 	$target = mysql_real_escape_string($_POST["Target"]);
+	echo "post escape test";
 	$query = "INSERT INTO Schedule (DOW, Start, End, Target) VALUES ('Mon', '00:00:00', '23:59:00', '$target')";
+	echo "query built";
 	$result=mysql_query($query);
+	echo "query ran";
 	
 	//echo $result;
 
