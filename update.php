22d89fde802ceadfd8f6dd2088fb9fcfe089c20f
Jase Payne <jase.payne@gmail.com>
2013-04-12 12:30:47 -0500

    fuck git

--- update.php
+++ update.php
@@ -1,18 +1,20 @@
 <?php
-	include("login.php");
-	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");	
-	echo "Connected <BR>";	
-	$selected=mysql_select_db($db) or die("Unable to select!");
+include("login.php");
+
+$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");	
+echo "Connected <BR>";	
+$selected=mysql_select_db($db) or die("Unable to select!");
 
 
-	$query="DELETE FROM `Schedule` WHERE `Schedule`.`PID` = 1";
-	$result=mysql_query($query);
+//$query="DELETE FROM Schedule";
+$query="DELETE FROM `Schedule` WHERE `Schedule`.`PID` = 1";
+$result=mysql_query($query);
 
-	$target = mysql_real_escape_string($_POST["Target"]);
-	echo "post escape test<BR>";
-	$query = "INSERT INTO `Schedule` (`PID`, `DOW`, `Start`, `Stop`, `Target`) VALUES ('1', 'Mon', '00:01:00', '11:59:59', '$target');";
-	echo "query built<BR>";
-	$result=mysql_query($query);
-	echo "query ran<BR>";
+$target = mysql_real_escape_string($_POST["Target"]);
+echo "post escape test<BR>";
+$query = "INSERT INTO `Schedule` (`PID`, `DOW`, `Start`, `Stop`, `Target`) VALUES ('1', 'Mon', '00:01:00', '11:59:59', '$target');";
+echo "query built<BR>";
+$result=mysql_query($query);
+echo "query ran<BR>";
 
 ?>
