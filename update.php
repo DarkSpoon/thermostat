4bdf3ad2f1e1d2c55f7fd7ebb14ca349236f4f83
DarkSpoon <jase.payne@gmail.com>
2013-04-11 20:21:53 -0500

    Connect, DELETE, grab target.

--- update.php
+++ update.php
@@ -9,9 +9,9 @@
 	$result=mysql_query($query);
 
 	echo "Just before the real escape";
-/*	$target =  mysqli::real_escape_string($_POST['Target']);
+	$target =  mysqli::real_escape_string($_POST['Target']);
 	echo $target;
-	$query = "INSERT INTO Schedule (DOW, Start, End, Target) VALUES ('Mon', '00:00:00', '23:59:00', '$target')";
+/*	$query = "INSERT INTO Schedule (DOW, Start, End, Target) VALUES ('Mon', '00:00:00', '23:59:00', '$target')";
 	$result=mysql_query($query);
 	
 	//echo $result;
