526aa373d7f0ccafcd5d95e98075bd1561da6d4c
DarkSpoon <jase.payne@gmail.com>
2013-04-11 21:15:36 -0500

    add pull hyperlink to update.php

--- update.php
+++ update.php
@@ -10,12 +10,13 @@
 	$result=mysql_query($query);
 
 	$target = mysql_real_escape_string($_POST["Target"]);
-	echo "post escape test";
+	echo "post escape test<BR>";
 	$query = "INSERT INTO `Schedule` (`PID`, `DOW`, `Start`, `Stop`, `Target`) VALUES ('1', 'Mon', '00:01:00', '11:59:59', '$target');";
-	echo "query built";
+	echo "query built<BR>";
 	$result=mysql_query($query);
-	echo "query ran";
+	echo "query ran<BR>";
 	
+	echo "<a src=\"pull.php\">Pull data</a>";
 	//echo $result;
 
 ?>
