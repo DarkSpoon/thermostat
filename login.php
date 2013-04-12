863691fcd62e0914c2da72ccec282565a113b9e6
DarkSpoon <jase.payne@gmail.com>
2013-04-11 19:07:44 -0500

    Add login.php includes
    Allow for db credentials to be used globally via an include

--- login.php
+++ login.php
@@ -1,10 +1,11 @@
 <?php
 	$un="pi";
 	$pw="raspberry";
-	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");
+	$db="thermostat";
+	//$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");
 	
-	echo "Connected <BR>";
+	//echo "Connected <BR>";
 	
-	$selected=mysql_select_db(thermostat) or die("Unable to select!");
+	//$selected=mysql_select_db(thermostat) or die("Unable to select!");
 
 ?>
--- pull.php
+++ pull.php
@@ -1,12 +1,12 @@
 <?php
-	//include login.php
-	$un="pi";
-	$pw="raspberry";
+	include(login.php);
+	//$un="pi";
+	//$pw="raspberry";
 	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");
 	
 	echo "Connected <BR>";
 	
-	$selected=mysql_select_db(thermostat) or die("Unable to select!");
+	$selected=mysql_select_db($db) or die("Unable to select!");
 	
 	$query="SELECT * from Conditions";
 	$result=mysql_query($query);
