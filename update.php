15c46f7ca0ef308c76568908c267ef0c021ad0f3
DarkSpoon <jase.payne@gmail.com>
2013-04-14 17:24:10 -0500

    new index elements

--- update.php
+++ update.php
@@ -24,11 +24,10 @@ echo "Connected <BR>";
 $selected=mysql_select_db($db) or die("Unable to select!");
 
 
-//$query="DELETE FROM Schedule";
-$query="DELETE FROM `Schedule` WHERE `Schedule`.`PID` = 1";
-$result=mysql_query($query);
-
 $target = mysql_real_escape_string($_POST["Target"]);
+$dow = 
+$start=
+$end=
 echo "post escape test<BR>";
 $query = "INSERT INTO `Schedule` (`PID`, `DOW`, `Start`, `Stop`, `Target`) VALUES ('1', 'Mon', '00:01:00', '11:59:59', '$target');";
 echo "query built<BR>";
