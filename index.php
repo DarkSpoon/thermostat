5566f2f51a72fbd8ff729d691248b09ba622014e
Jase Payne <jase.payne@gmail.com>
2013-04-15 11:42:57 -0500

    revert to old var name

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ echo " <body>";
 echo " </body>";
 echo "</html>";
 
-if (isset($_POST['Submit'])){
+if (isset($_POST['submit'])){
  
 	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");	
 	echo "Connected <BR>";	
