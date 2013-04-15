d214fa616a7f716c962e8bbe4494a3a678ae1644
Jase Payne <jase.payne@gmail.com>
2013-04-15 11:52:22 -0500

    Fix isser() parens

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ echo " <body>";
 
 echo " </body>";
 echo "</html>";
-echo isset($_POST['Target']; 
-echo isset($_POST["Target"];
-if (isset($_POST['Target'])){
+echo isset($_POST['Target']); 
+echo isset($_POST["Target"]);
+/*if (isset($_POST['Target'])){
  
 	$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");	
 	echo "Connected <BR>";	
@@ -38,5 +38,5 @@ if (isset($_POST['Target'])){
 	echo "query built<BR>";
 	$result=mysql_query($query);
 	echo "query ran<BR>";
-}
+}*/
 ?>
