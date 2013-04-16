f3c1ad3e768885c39ba0b9b298191f264e7e49dc
Jase Payne <jase.payne@gmail.com>
2013-04-16 11:56:19 -0500

    clean up pull.php

--- pull.php
+++ pull.php
@@ -41,7 +41,6 @@ Released   : 20081230
           	<?php
 				include("login.php");
 				$dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");	
-				echo "Connected <BR>";	
 				$selected=mysql_select_db($db) or die("Unable to select!");
 
 
@@ -50,25 +49,29 @@ Released   : 20081230
 				$result=mysql_query($query);
 
 				while($row=mysql_fetch_array($result)){
-				echo "<BR>Average temp from query ".$row{'Temp'}."F<BR>";
+				echo "Currently: ".$row{'Temp'}."F<BR>";
 				$AC=$row{'AC'};
 				$Heat=$row{'Heat'};
 				$Fan=$row{'Fan'};
 
 				#Check for nothing running
 				if($AC==1)
-				echo "AC is running<BR>";
+					echo "AC is running<BR>";
 				else if($Heat==1)
-				echo "Heat is running<BR>";
+					echo "Heat is running<BR>";
+				else if($AC==0 && $Heat==0)
+					echo "System is idle<BR>";
 				if ($Fan==1)
-				echo "Fan is running<BR>";
+					echo "Fan is running<BR>";
+				if ($Fan==0)
+					echo "Fan is on auto<BR>";
 
 				}
 
 				$query="SELECT Target from Conditions";
 				$result=mysql_query($query);
 				while($row=mysql_fetch_array($result)){
-				echo "Target temp from query ".$row{'Target'}."F<BR>";
+				echo "Target: ".$row{'Target'}."F<BR>";
 				}
 
 				mysql_close($dbhandle);
