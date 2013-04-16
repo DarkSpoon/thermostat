c68b1840204be8c16708f57d77c6147dda05a29a
Jase Payne <jase.payne@gmail.com>
2013-04-16 17:04:44 -0500

    back to buttons

--- schedule.php
+++ schedule.php
@@ -177,9 +177,9 @@ Released   : 20081230
 					echo "<input type=\"text\" value=\"$start\" name=\"Start$pid\"> ";
 					echo "<input type=\"text\" value=\"$stop\" name=\"Stop$pid\"> ";
 					echo "<input type=\"text\" value=\"$target\" name=\"SchedTarget$pid\"> ";
-					//echo "<input type=\"submit\" value=\"Save\"> ";
+					echo "<input type=\"submit\" value=\"Save\"> ";
 					echo "<a href=\"schedule.php?d=$pid\"><img border=\"0\" src=\"delete.png\" align=\"absmiddle\" alt=\"Delete entry\" width=\"23\" height=\"20\"> </a>";
-					echo "<a href=\"schedule.php?u=$pid\"><img border=\"0\" src=\"save.png\" align=\"absmiddle\" alt=\"Update entry\" width=\"23\" height=\"20\"> </a>";
+					//echo "<a href=\"schedule.php?u=$pid\"><img border=\"0\" src=\"save.png\" align=\"absmiddle\" alt=\"Update entry\" width=\"23\" height=\"20\"> </a>";
 					echo "</form> ";
 					//echo "<BR>";
 				}
