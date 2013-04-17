b2add73d887063ee3afc7775ce935905669cdfb7
Jase Payne <jase.payne@gmail.com>
2013-04-17 16:23:28 -0500

    login pages

--- login.php
+++ login.php
@@ -1,5 +1,12 @@
+<script type="text/javascript" src="sha512.js"></script>
+<script type="text/javascript" src="forms.js"></script>
 <?php
-	$un="pi";
-	$pw="raspberry";
-	$db="thermostat";
+if(isset($_GET['error'])) { 
+   echo 'Error Logging In!';
+}
 ?>
+<form action="process_login.php" method="post" name="login_form">
+   Email: <input type="text" name="email" /><br />
+   Password: <input type="password" name="password" id="password"/><br />
+   <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
+</form>
