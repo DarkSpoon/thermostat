dd0121c42532ba4c28713778ad78ba8e5d614206
DarkSpoon <jase.payne@gmail.com>
2013-04-17 11:19:17 -0500

    format output on index

--- index.php
+++ index.php
@@ -45,32 +45,48 @@ Released   : 20081230
               $selected=mysql_select_db($db) or die("Unable to select!");
 
 
+              $output="";#output string to be echoed
+
+              $query="SELECT * from User_Req";
+              $result=mysql_query($query);
+              #Build strings to display what is currently running. 
+              while$row=mysql_fetch_array($result)){
+                $AC=$row{'AC'};
+                $Heat=$row{'Heat'};
+                $Fan=$row{'Fan'};
+
+                if($AC==1)
+                  $output=$output+"Cool, ";
+                else if($Heat==1)
+                  $output=$output+"Heat, ";
+                if($Fan==0)
+                  $output=$output+"Auto <BR>";
+                else if($Fan==1)
+                  $output=$output+"On <BR>";
+              }
 
               $query="SELECT * from Conditions";
               $result=mysql_query($query);
-
               while($row=mysql_fetch_array($result)){
-              echo "Currently: ".$row{'Temp'}."F<BR>";
-              $AC=$row{'AC'};
-              $Heat=$row{'Heat'};
-              $Fan=$row{'Fan'};
-              
-              #Display what is currently running. 
-              #The below values will be used to trigger relays and should reflect an accurate status
-              if($AC==1)
-                echo "AC is running<BR>";
-              else if($Heat==1)
-                echo "Heat is running<BR>";
-              else if($AC==0 && $Heat==0)
-                echo "System is idle<BR>";
-              if ($Fan==1)
-                echo "Fan is running<BR>";
-              if ($Fan==0)
-                echo "Fan is on auto<BR>";
-              echo "Target: ".$row{'Target'}."F<BR>";
-
+                $output=$output + "Currently: ".$row{'Temp'}."F<BR>";
+                $AC=$row{'AC'};
+                $Heat=$row{'Heat'};
+                $Fan=$row{'Fan'};
+                
+                #The below values will be used to trigger relays in server code and should reflect an accurate status
+                if($AC==1)
+                  $output=$output + "AC is running<BR>";
+                else if($Heat==1)
+                  $output=$output + "Heat is running<BR>";
+                else if($AC==0 && $Heat==0)
+                  $output=$output + "System is idle<BR>";
+                if ($Fan==1)
+                  $output=$output + "Fan is running<BR>";
+                else if ($Fan==0)
+                  $output=$output + "Fan is on auto<BR>";
+                $output=$output + "Target: ".$row{'Target'}."F<BR>";
               }
-
+              echo $output;
               //mysql_close($dbhandle);
 
             ?>
