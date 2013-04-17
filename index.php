72011edbfd5539e07196e4a1423875fe054f4e7b
DarkSpoon <jase.payne@gmail.com>
2013-04-17 12:03:39 -0500

    move $dbhandler and $selected outside the comparison

--- index.php
+++ index.php
@@ -20,41 +20,40 @@ Released   : 20081230
 <title>Thermostat</title>
 </head>
 <?php
-    if ($_GET['w']==1){
-        include("login.php");
-        $dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");      
-        $selected=mysql_select_db($db) or die("Unable to select!");
+  include("login.php");
+  $dbhandle=mysql_connect(localhost,$un,$pw) or die("Unable to connect!");      
+  $selected=mysql_select_db($db) or die("Unable to select!");
+  if ($_GET['w']==1){
+      #Set manual overrides
+      $hvac=mysql_real_escape_string($_POST["HVAC"]);
+      $fan=mysql_real_escape_string($_POST["fan"]);
+      if(isset($_POST["Target"])){
+        $target = mysql_real_escape_string($_POST["Target"]);
+      } 
+      else {
+        $target=0;
+      }
+      
+      if ($hvac=="heat"){
+        $heater=1;
+        $ac=0;
+      }
+      elseif ($hvac=="ac"){
+        $heater=0;
+        $ac=1;
+      }
+      $query = "UPDATE User_Req SET Heater=$heater, AC=$ac, Fan=$fan, Temp=$target";
+      $result=mysql_query($query);
 
-        #Set manual overrides
-        $hvac=mysql_real_escape_string($_POST["HVAC"]);
-        $fan=mysql_real_escape_string($_POST["fan"]);
-        if(isset($_POST["Target"])){
-          $target = mysql_real_escape_string($_POST["Target"]);
-        } 
-        else {
-          $target=0;
-        }
-        
-        if ($hvac=="heat"){
-          $heater=1;
-          $ac=0;
-        }
-        elseif ($hvac=="ac"){
-          $heater=0;
-          $ac=1;
-        }
-        $query = "UPDATE User_Req SET Heater=$heater, AC=$ac, Fan=$fan, Temp=$target";
-        $result=mysql_query($query);
+      mysql_close($dbhandle);
+  }
+  if ($_GET['w']==2){
+      $query = "UPDATE User_Req SET Temp=0";
+      $result=mysql_query($query);
 
-        mysql_close($dbhandle);
-    }
-    if ($_GET['w']==2){
-        $query = "UPDATE User_Req SET Temp=0";
-        $result=mysql_query($query);
-
-        mysql_close($dbhandle);
-    }
-  ?>
+      mysql_close($dbhandle);
+  }
+?>
 <body>
     <div id="page">
     
