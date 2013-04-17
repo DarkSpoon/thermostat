a25f04795487a0bd485e3e5b84cc194959c772e9
Jase Payne <jase.payne@gmail.com>
2013-04-17 16:29:58 -0500

    fix db name in includes

--- includes.php
+++ includes.php
@@ -1,4 +1,4 @@
 <?php
 	$dbhandle=mysql_connect(localhost,"pi","raspberry") or die("Unable to connect!");      
-  	$selected=mysql_select_db("terhmostat") or die("Unable to select!");
+  	$selected=mysql_select_db("thermostat") or die("Unable to select!");
 ?>
