6df376efbb1ead590cd1077176fb8e9fe9604d86
DarkSpoon <jase.payne@gmail.com>
2013-04-17 12:37:12 -0500

    remove echo test, empty() working

--- index.php
+++ index.php
@@ -46,11 +46,9 @@ Released   : 20081230
             
             if(empty($_POST["Target"])){              
               $target = 0;
-              echo $target;
             } 
             else {
               $target = mysql_real_escape_string($_POST["Target"]);
-              echo $target;
             }
             
             if ($hvac=="heat"){
