cb56f9d426e999390f13f37824bbbb7de7191f74
DarkSpoon <jase.payne@gmail.com>
2013-04-16 20:18:40 -0500

    move images to folder
    so cluttered

--- schedule.php
+++ schedule.php
@@ -178,7 +178,7 @@ Released   : 20081230
 					echo "<input type=\"text\" value=\"$stop\" name=\"Stop$pid\"> ";
 					echo "<input type=\"text\" value=\"$target\" name=\"SchedTarget$pid\"> ";
 					echo "<input type=\"submit\" value=\"Save\"> ";
-					echo "<a href=\"schedule.php?d=$pid\"><img border=\"0\" src=\"delete.png\" align=\"absmiddle\" alt=\"Delete entry\" width=\"23\" height=\"20\"> </a>";
+					echo "<a href=\"schedule.php?d=$pid\"><img border=\"0\" src=\"images/delete.png\" align=\"absmiddle\" alt=\"Delete entry\" width=\"23\" height=\"20\"> </a>";
 					//echo "<a href=\"schedule.php?u=$pid\"><img border=\"0\" src=\"save.png\" align=\"absmiddle\" alt=\"Update entry\" width=\"23\" height=\"20\"> </a>";
 					echo "</form> ";
 					//echo "<BR>";
