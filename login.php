d9dc2cf3ecd6f2cb275690ce9592cbbc67cf17f5
DarkSpoon <jase.payne@gmail.com>
2013-04-17 19:42:07 -0500

    move script tags

--- login.php
+++ login.php
@@ -4,8 +4,6 @@
   include("functions.php");
   sec_session_start();
 ?>
-<script type="text/javascript" src="sha512.js"></script>
-<script type="text/javascript" src="forms.js"></script>
  <!--
 Design by Bryant Smith
 http://www.bryantsmith.com
@@ -22,6 +20,8 @@ Released   : 20081230
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<script type="text/javascript" src="sha512.js"></script>
+<script type="text/javascript" src="forms.js"></script>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="style.css" />
