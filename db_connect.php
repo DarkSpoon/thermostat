63790d9d70f8e0ac51c875c544b6055ecf2fd14d
DarkSpoon <jase.payne@gmail.com>
2013-04-25 13:48:07 -0500

    semicolon

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@ define("HOST", "localhost"); // The host you want to connect to.
 define("USER", "pi"); // The database username.
 define("PASSWORD", "raspberry"); // The database password. 
 define("DATABASE", "secure_login"); // The database name.
-define("DATABASE2", "thermostat")
+define("DATABASE2", "thermostat");
 
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
