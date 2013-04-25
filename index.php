5f3ce81155d84754d71fa4f4bcb57cfda791c3db
Jase Payne <jase.payne@gmail.com>
2013-04-25 16:50:35 -0500

    uncomment w==1 comment out w==2

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ Released   : 20081230
         if(login_check($mysqli) == true) {
           
 
-          /*
+          
           if ($_GET['w']==1){
               #Set manual overrides for Heat, AC, and Fan
               $hvac=mysqli_real_escape_string($selected, $_POST["HVAC"]);
@@ -76,7 +76,7 @@ Released   : 20081230
           }
 
 
-          */
+          /*
           if ($_GET['w']==2){#reset temp to scheduled temp
               //$query = "UPDATE User_Req SET Temp=0";
               //$result=mysql_query($query);
@@ -88,7 +88,7 @@ Released   : 20081230
           }
 
 
-          
+            */          
           if ($_GET['w']==3){#set manual override for temp
               if(empty($_POST["Target"])){              
                 $target = 0;
