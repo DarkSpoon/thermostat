7404e971325cceae444df97b6c8c38ed397ba2ff
Jase Payne <jase.payne@gmail.com>
2013-04-25 12:53:32 -0500

    more db_connect tweaks

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,13 @@
 <?php
-define("HOST", "localhost"); // The host you want to connect to.
+/*define("HOST", "localhost"); // The host you want to connect to.
 define("USER", "pi"); // The database username.
 define("PASSWORD", "raspberry"); // The database password. 
 define("DATABASE", "secure_login"); // The database name.
-define("DATABASE2", "thermostat")
+define("DATABASE2", "thermostat")*/
 
-$con = new mysqli(HOST, USER, PASSWORD);
-$mysqli = mysqli_select_db($con, DATABASE);
-$selected = mysqli_select_db($con, DATABASE2);
+$con = new mysqli("localhost", "pi", "raspberry");
+$mysqli = mysqli_select_db($con, "secure_login");
+$selected = mysqli_select_db($con, "thermostat");
 
 if(mysqli_connect_errno()) {
       echo "Connection Failed: " . mysqli_connect_errno();
