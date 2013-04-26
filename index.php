edfd82a48625ff8ef1c2d401fff316ae314bd4bd
DarkSpoon <jase.payne@gmail.com>
2013-04-25 19:15:20 -0500

    change bind type to int

--- index.php
+++ index.php
@@ -101,8 +101,7 @@ Released   : 20081230
               //$result=mysql_query($query);
 
               if($statement = $selected->prepare("UPDATE User_Req SET Temp = ?")){
-                echo $target;
-                if($statement->bind_param('f', $target)){
+                if($statement->bind_param('i', $target)){
                   echo "3, bound";
                 }else echo "3, not bound";
                 $statement->execute();
