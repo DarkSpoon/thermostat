633ebcab30363c0c360361ff5dd9287d505e7b68
DarkSpoon <jase.payne@gmail.com>
2013-04-25 19:12:16 -0500

    echo testing w==3

--- index.php
+++ index.php
@@ -101,7 +101,9 @@ Released   : 20081230
               //$result=mysql_query($query);
 
               if($statement = $selected->prepare("UPDATE User_Req SET Temp = ?")){
-                $statement->bind_param('f', $target);
+                if($statement->bind_param('f', $target)){
+                  echo "3, bound";
+                }else echo "3, not bound";
                 $statement->execute();
                 $statement->close();
               }else echo "3, not prepared";
