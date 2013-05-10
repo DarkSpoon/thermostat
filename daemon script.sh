25f7396600288d009706ae1ad1bd8d2a67ec25b7
Jase Payne <jase.payne@gmail.com>
2013-05-10 11:55:30 -0500

    new daemon script

--- daemon script.sh
+++ daemon script.sh
@@ -1,64 +1,61 @@
 #!/bin/bash
-# myapp daemon
-# chkconfig: 345 20 80
-# description: myapp daemon
-# processname: myapp
 
-DAEMON_PATH="/var/www"
+# Source function library.
+. /etc/init.d/functions
 
-DAEMON=thermo-server
-DAEMONOPTS=""
 
-NAME=Thermostat
-DESC="My daemon description"
-PIDFILE=/var/run/$NAME.pid
-SCRIPTNAME=/etc/init.d/$NAME
+DIR=/var/www
+PIDFILE=$DIR/logs/pid
+STARTPIDFILE=$DIR/logs/startpid
+
+
+start() {
+        echo -n "Starting Thermostat: "
+        if [ -f $STARTPIDFILE ]; then
+                PID=`cat $STARTPIDFILE`
+                echo Thermostat already running: $PID
+                exit 2;
+        elif [ -f $PIDFILE ]; then
+                PID=`cat $PIDFILE`
+                echo Thermostat already running: $PID
+                exit 2;
+        else
+                cd $DIR
+                daemon  ./thermo-server
+                RETVAL=$?
+                echo
+                [ $RETVAL -eq 0 ] && touch /var/lock/subsys/thermo-server
+                return $RETVAL
+        fi
+
+}
+
+stop() {
+        echo -n "Shutting down Thermostat: "
+        echo
+        killproc thermo-server
+        echo
+        rm -f /var/lock/subsys/thermo-server
+        return 0
+}
 
 case "$1" in
-start)
-	printf "%-50s" "Starting $NAME..."
-	cd $DAEMON_PATH
-	PID=`$DAEMON $DAEMONOPTS > /dev/null 2>&1 & echo $!`
-	#echo "Saving PID" $PID " to " $PIDFILE
-        if [ -z $PID ]; then
-            printf "%s\n" "Fail"
-        else
-            echo $PID > $PIDFILE
-            printf "%s\n" "Ok"
-        fi
-;;
-status)
-        printf "%-50s" "Checking $NAME..."
-        if [ -f $PIDFILE ]; then
-            PID=`cat $PIDFILE`
-            if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
-                printf "%s\n" "Process dead but pidfile exists"
-            else
-                echo "Running"
-            fi
-        else
-            printf "%s\n" "Service not running"
-        fi
-;;
-stop)
-        printf "%-50s" "Stopping $NAME"
-            PID=`cat $PIDFILE`
-            cd $DAEMON_PATH
-        if [ -f $PIDFILE ]; then
-            kill -HUP $PID
-            printf "%s\n" "Ok"
-            rm -f $PIDFILE
-        else
-            printf "%s\n" "pidfile not found"
-        fi
-;;
-
-restart)
-  	$0 stop
-  	$0 start
-;;
-
-*)
-        echo "Usage: $0 {status|start|stop|restart}"
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    status)
+        status ns-slapd
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    *)
+        echo "Usage:  {start|stop|status|restart}"
         exit 1
+        ;;
 esac
+exit $?
